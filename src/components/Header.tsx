9f14cdb597aad7c1cf1e4cbe179b272ca7cedfef
Ulises Sanchez <wulisesbusiness@gmail.com>
2025-08-14 11:55:27 -0600

    Ultimo commit para sacarlo a producción gracias Haru

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@
 export default function Header() {
   return (
     <header className="h-16 bg-darkBlack text-creamWhite flex items-center justify-between px-6 border-b border-haruPurple">
-      <h1 className="text-lg font-serif text-haruPurple">Bienvenido de nuevo, HaruFan</h1>
+      <h1 className="text-lg font-serif text-haruPurple">Bienvenido de nuevo!!!</h1>
       <div className="text-sm text-haruPurple">Modo oscuro [activo]</div>
     </header>
   )
--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -6,15 +6,9 @@ export default function SidebarMakoto() {
 
         <aside className='w-60 h-screen bg-softCard text-creamWhite p-4 fixed top-0 left-0'>
 
-            <h2 className='text-xl font-serif text-haruPurple mb-8'>Haru Finance</h2>
+            <h2 className='text-xl font-serif text-haruPurple mb-8'>Finance</h2>
 
             <nav className='flex flex-col gap-4 text-sm' >
-
-                <Link  href='#' className='hover:text-personaRed transition-colors'> Makoto </Link>
-                <Link  href='#' className='hover:text-personaRed transition-colors'> Kasumi </Link>
-                <Link  href='#' className='hover:text-personaRed transition-colors'> Sumire </Link>
-                <Link  href='#' className='hover:text-personaRed transition-colors'> Aigis </Link>
-                <Link  href='#' className='hover:text-personaRed transition-colors'> Yukari </Link>
 
 
             </nav>
