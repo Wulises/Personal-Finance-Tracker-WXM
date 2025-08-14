254529711fe7f98c81d2b470a1aabe17cc31bc93
Ulises Sanchez <wulisesbusiness@gmail.com>
2025-08-14 11:58:47 -0600

    Ultimo commit para sacarlo a producción gracias Haru

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,15 +1,12 @@
-import Link from 'next/link'
-
 export default function SidebarMakoto() {
 
     return ( 
 
         <aside className='w-60 h-screen bg-softCard text-creamWhite p-4 fixed top-0 left-0'>
 
-            <h2 className='text-xl font-serif text-haruPurple mb-8'>Finance</h2>
+            <h2 className='text-xl font-serif text-haruPurple mb-8'>Haru Finance</h2>
 
             <nav className='flex flex-col gap-4 text-sm' >
-
 
             </nav>
 
